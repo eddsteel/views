0989bd4f59afb12e42d2021a2bd55cdd53f7e207
Edd Steel <edward.steel@gmail.com>
2018-07-29 10:48:48 -0700

    Remove generated test

--- src/test/scala/MainTest.scala
+++ src/test/scala/MainTest.scala
@@ -1,8 +1,4 @@
 package com.eddsteel.views
 import org.scalatest.FlatSpec
 
-class MainTest extends FlatSpec {
-  "main" should "provide a cool cat" in {
-    assert(Main.cat === "😸")
-  }
-}
+class MainTest extends FlatSpec {}
